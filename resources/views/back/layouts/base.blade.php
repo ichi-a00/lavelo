eede17c0a7b9326cada35ed5c4665aeb03387ecc
ichi-a00 <prenehru@gmail.com>
2022-01-11 15:32:32 +0000

    [fix]bootstrap

--- resources/views/back/layouts/base.blade.php
+++ resources/views/back/layouts/base.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ route('back.dashboard') }}">
                 管理画面
